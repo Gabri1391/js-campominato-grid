8062d81e08e6276ca1abaf223f722b9824d18097
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-30 17:59:29 +0200

    milestone 3 done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 //MILESTONE 2
 //Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
 
+// FUNZIONI
+const  createCell = content =>{
+    const cell = document.createElement('div'); 
+    cell.innerText = content;
+    cell.className = 'cell';
+    grid.appendChild(cell);
+
+    return cell;
+}
+
 // - Recupero l'elemento griglia e il bottone dal DOM
 const grid = document.getElementById('grid');
 const button = document.getElementById('button');
@@ -14,17 +24,14 @@ const totalCells = row * cells;
 
 // -Ciclo for per creare 100 celle all'interno della griglia
 
-for(i = 0; i < totalCells; i++){
-    const cell = document.createElement('div');
+for (i = 1; i <= totalCells; i++) {
+    const newCell = createCell(i);
 
-    cell.className = 'cell';
-
-    grid.appendChild(cell);
 }
 
 // - Aggiungo l'evento al click del bottone
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
 
     grid.classList.remove('none');
     grid.classList.add('active');
